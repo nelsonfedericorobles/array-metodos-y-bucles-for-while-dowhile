3b77a1f1e38890bd90b7fc93a3260a89fb97853f
Nelson Robles <nelsonfedericorobles@gmail.com>
2023-05-14 14:02:07 -0300

    fin de for

---
+++ JS/for-of.js
@@ -0,0 +1,6 @@
+const paisesLatinos = ["Argentina", "Bahamas", "Barbados", "Belice", "Bolivia", "Brasil" , "Colombia", "Costa Rica", "Cuba", "Chile", "Dominica", "Ecuador", "El Salvador", "Granada", "Guatemala", "Guyana", "Haití", "Honduras", "Jamaica", "México", "Nicaragua", "Panamá", "Paraguay", "Perú", "República", "Dominicana"]
+
+
+for (let paises of paisesLatinos) {
+    console.log(paises)
+}
